963ef0ba1afb293ca31a185feb0fc57d66d443af
SKolodkin <dev1@instrument-fit.ru>
2023-10-12 15:12:24 +0300

    create class HomePageTest

--- superlists/todo/tests.py
+++ superlists/todo/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from .views import home_page
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/todo/')
+        self.assertEqual(found.func, home_page)
