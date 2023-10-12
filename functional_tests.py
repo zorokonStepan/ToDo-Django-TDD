13cf57bc4a397088f204d2e8a963eec382f2ff76
SKolodkin <dev1@instrument-fit.ru>
2023-10-12 08:47:21 +0300

    create class NewVisitorTest

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,22 @@
+import unittest
+
 from selenium import webdriver
 
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Congratulations!' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+        self.fail("Finish test!")
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
