d2a73ed9590d4c10efd19f57a6fb308ae74dfa29
SKolodkin <dev1@instrument-fit.ru>
2023-10-17 08:46:31 +0300

    replace tests (test_root_url_resolves_to_home_page_view and test_home_page_returns_correct_html) -> test_uses_home_template

--- superlists/todo/tests.py
+++ superlists/todo/tests.py
@@ -1,21 +1,8 @@
 from django.test import TestCase
-from django.urls import resolve
-from django.http import HttpRequest
-
-from .views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/todo/')
-        self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         response = self.client.get('/todo/')
-        html = response.content.decode('utf8')
-
-        self.assertTrue(html.strip().startswith('<html>'))
-        self.assertIn('<title>To-Do list</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
         self.assertTemplateUsed(response, 'todo/home.html')
