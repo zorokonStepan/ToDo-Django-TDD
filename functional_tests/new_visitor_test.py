5fef8e22b053a30873ec843196fbc0e8f64f5212
SKolodkin <dev1@instrument-fit.ru>
2023-10-18 09:03:23 +0300

    change NewVisitorTest.test_can_start_a_list_and_retrieve_it_later

--- functional_tests/new_visitor_test.py
+++ functional_tests/new_visitor_test.py
@@ -16,6 +16,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     @print_finish_message
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8000/todo/')
@@ -24,18 +29,17 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
+        # input_box.send_keys(Keys.ENTER)
+        # time.sleep(1)
+        # self.check_for_row_in_list_table('1: Buy peacock feathers')
+
         input_box = self.browser.find_element(By.ID, 'id_new_item')
-        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
-
-        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys('Make a fly from peacock feathers')
         input_box.send_keys(Keys.ENTER)
-
         time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Make a fly from peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Make a fly from peacock feathers')
 
 
 if __name__ == "__main__":
