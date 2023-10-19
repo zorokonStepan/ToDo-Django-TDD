8501b66462830c72c16b982b33e7989aef426c09
SKolodkin <dev1@instrument-fit.ru>
2023-10-19 08:44:54 +0300

    change def home_page and test_can_save_a_POST_request

--- superlists/todo/tests/tests_views/tests.py
+++ superlists/todo/tests/tests_views/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 
+from ...models import Item
+
 
 class HomePageTest(TestCase):
 
@@ -9,5 +11,10 @@ class HomePageTest(TestCase):
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/todo/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'todo/home.html')
--- superlists/todo/views.py
+++ superlists/todo/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from .models import Item
+
 
 def home_page(request: HttpRequest) -> HttpResponse:
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
     return render(request, 'todo/home.html', {
         'new_item_text': request.POST.get('item_text', ''),
     })
-
-    # if request.method == "POST":
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request, 'todo/home.html')
