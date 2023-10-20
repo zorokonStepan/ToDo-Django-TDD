6f04f195f85befc9fdb55c19a70c8297bb14c7d4
SKolodkin <dev1@instrument-fit.ru>
2023-10-20 08:44:19 +0300

    change def test_can_save_a_POST_request and def home_page

--- superlists/todo/views.py
+++ superlists/todo/views.py
@@ -1,13 +1,12 @@
 from django.http import HttpResponse, HttpRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Item
 
 
 def home_page(request: HttpRequest) -> HttpResponse:
     if request.method == "POST":
-        new_item_text = request.POST['item_text']
-        Item.objects.create(text=new_item_text)
-    else:
-        new_item_text = ''
-    return render(request, 'todo/home.html', {'new_item_text': new_item_text})
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/todo/')
+
+    return render(request, 'todo/home.html')
