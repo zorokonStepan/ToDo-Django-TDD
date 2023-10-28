087e5d1e46fd9bcec05b63e180b8ef1433ce09b1
Stepan Kolodkin <zorokon@yandex.ru>
2023-10-28 21:34:17 +0300

    create NewVisitorTest.wait_for_row_in_list_table

--- superlists/functional_tests/test_new_visitor.py
+++ superlists/functional_tests/test_new_visitor.py
@@ -3,10 +3,12 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
 
 
 class NewVisitorTest(LiveServerTestCase):
+    MAX_WAIT = 10
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -14,10 +16,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return None
+            except (AssertionError, WebDriverException) as exception:
+                if time.time() - start_time > self.MAX_WAIT:
+                    raise exception
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get(f'{self.live_server_url}/todo/')
@@ -29,14 +39,12 @@ class NewVisitorTest(LiveServerTestCase):
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Buy peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         input_box = self.browser.find_element(By.ID, 'id_new_item')
         input_box.send_keys('Make a fly from peacock feathers')
         input_box.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Make a fly from peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Make a fly from peacock feathers')
