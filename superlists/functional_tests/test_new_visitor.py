28d189e7ca361187bef0a98aa71f84c860e728dd
Stepan Kolodkin <zorokon@yandex.ru>
2023-10-29 15:00:43 +0300

    create NewVisitorTest.test_multiple_users_can_start_lists_at_different_urls

--- superlists/functional_tests/test_new_visitor.py
+++ superlists/functional_tests/test_new_visitor.py
@@ -29,7 +29,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise exception
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         self.browser.get(f'{self.live_server_url}/todo/')
 
         self.assertIn('To-Do', self.browser.title)
@@ -48,3 +48,37 @@ class NewVisitorTest(LiveServerTestCase):
 
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Make a fly from peacock feathers')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        self.browser.get(f'{self.live_server_url}/todo/')
+
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Make a fly', page_text)
+
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        input_box.send_keys('Buy milk')
+        input_box.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
