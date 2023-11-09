c47444853666c1a8f6b602e84c04a730f1a44484
SKolodkin <dev1@instrument-fit.ru>
2023-11-09 15:45:51 +0300

    change urls

--- superlists/superlists/urls.py
+++ superlists/superlists/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from todo.views import home_page
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home_page, name="home"),
     path('todo/', include('todo.urls')),
 ]
--- superlists/todo/urls.py
+++ superlists/todo/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import home_page, view_list, new_list, add_item
+from .views import view_list, new_list, add_item
 
 
 urlpatterns = [
-    path('', home_page, name="home"),
     path("new", new_list, name="new_list"),
     path("<int:list_id>/", view_list, name="view_list"),
     path("<int:list_id>/add_item", add_item, name="add_item"),
