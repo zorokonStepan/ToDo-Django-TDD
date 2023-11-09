3d2fbef884abd2ad07ebeb020fc973bcc70525e6
SKolodkin <dev1@instrument-fit.ru>
2023-11-09 15:39:34 +0300

    replace LiveServerTestCase to StaticLiveServerTestCase

--- superlists/functional_tests/test_new_visitor.py
+++ superlists/functional_tests/test_new_visitor.py
@@ -4,7 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
