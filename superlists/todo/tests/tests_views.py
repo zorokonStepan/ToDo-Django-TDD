844d7e07ddc70eaa51de95543847ddbbd8b8eda5
SKolodkin <dev1@instrument-fit.ru>
2023-11-10 09:05:45 +0300

    Provided model validation in list view

--- superlists/todo/tests/tests_views.py
+++ superlists/todo/tests/tests_views.py
@@ -61,29 +61,28 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/todo/{correct_list.id}/")
         self.assertEqual(response.context["list"], correct_list)
 
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
-        _ = List.objects.create()
+        List.objects.create()
         correct_list = List.objects.create()
 
-        self.client.post(
-            f"/todo/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
+        self.client.post(f"/todo/{correct_list.id}/", data={"item_text": "A new item for an existing list"})
 
         self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.get()
         self.assertEqual(new_item.text, "A new item for an existing list")
         self.assertEqual(new_item.list, correct_list)
 
-    def test_redirects_to_list_view(self):
-        _ = List.objects.create()
+    def test_POST_redirects_to_list_view(self):
+        List.objects.create()
         correct_list = List.objects.create()
 
-        response = self.client.post(
-            f"/todo/{correct_list.id}/add_item",
-            data={"item_text": "A new item for an existing list"},
-        )
-
+        response = self.client.post(f"/todo/{correct_list.id}/", data={"item_text": "A new item for an existing list"})
         self.assertRedirects(response, f"/todo/{correct_list.id}/")
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(f"/todo/{list_.id}/", data={"item_text": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "todo/list.html")
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
--- superlists/todo/urls.py
+++ superlists/todo/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from .views import view_list, new_list, add_item
+from .views import view_list, new_list
 
 
 urlpatterns = [
     path("new", new_list, name="new_list"),
     path("<int:list_id>/", view_list, name="view_list"),
-    path("<int:list_id>/add_item", add_item, name="add_item"),
 ]
