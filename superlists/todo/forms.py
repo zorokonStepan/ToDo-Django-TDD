0776b419b043ab4613fcab2a4678ecf0fad57c4d
SKolodkin <dev1@instrument-fit.ru>
2023-11-10 14:26:16 +0300

    New form for list items

---
+++ superlists/todo/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+from .models import Item
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+    class Meta:
+        model = Item
+        fields = ("text",)
+        widgets = {
+            "text": forms.widgets.TextInput(
+                attrs={
+                    "placeholder": "Enter a to-do item",
+                    "class": "form-control form-control-lg",
+                }
+            ),
+        }
+        error_messages = {"text": {"required": EMPTY_ITEM_ERROR}}
+
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
---
+++ superlists/todo/tests/tests_forms.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+
+from ..models import Item, List
+from ..forms import EMPTY_ITEM_ERROR, ItemForm
+
+
+class ItemFormTest(TestCase):
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        form = ItemForm()
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+        self.assertIn('class="form-control form-control-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        form = ItemForm(data={"text": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors["text"], [EMPTY_ITEM_ERROR])
+
+    def test_form_save_handles_saving_to_a_list(self):
+        mylist = List.objects.create()
+        form = ItemForm(data={"text": "do me"})
+        new_item = form.save(for_list=mylist)
+        self.assertEqual(new_item, Item.objects.get())
+        self.assertEqual(new_item.text, "do me")
+        self.assertEqual(new_item.list, mylist)
